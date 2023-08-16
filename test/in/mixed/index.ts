bdf11c0c04abeea2cbf0bba314480a4b88c59dd6
Barry Gribben <bgribben@gmail.com>
2023-08-16 18:47:09 +0100

    Add SCSS support

---
+++ test/in/mixed/index.ts
@@ -0,0 +1,9 @@
+import css from './css.module.css'
+import scss from './scss.module.scss'
+import sass from './sass.module.sass'
+
+const a = css
+const b = scss
+const c = sass
+
+console.log(a, b, c)
---
+++ test/in/sass/index.ts
@@ -0,0 +1,5 @@
+import styles from './styles.module.sass'
+
+const c = styles
+
+console.log(c)
